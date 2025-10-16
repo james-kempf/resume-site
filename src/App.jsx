cb2897d84322ad8c8d69e5879f0eb0d9ea134af4
James Kempf <jamesk4321@gmail.com>
2025-10-16 18:46:03 -0500

    move info to data folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,10 @@
 import { useState } from 'react'
 import portrait from './assets/portrait.jpg'
+import sections from './data/sections.json'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
-  const sections = [
-    {
-      id: 0,
-      title: "Experience",
-      entries: [
-        {
-          id: 0,
-          name: 'Thentia',
-          website: 'https://thentia.com',
-          image: 'src/assets/thentia.svg',
-          subtitle: 'Full Stack Developer',
-          date: 'February 2023 - October 2025',
-          description: 'As a Full Stack Developer, I worked closely with clients to troubleshoot, customize, and resolve issues in highly specialized software applications. Leveraging JavaScript, AngularJS, PHP, and Java, I developed and implemented creative solutions tailored to unique business requirements. Partnered with cross-functional teams to ensure timely resolution and continuous improvement of client systems, while maintaining strong communication to understand evolving needs and deliver high-impact results.',
-        },
-        {
-          id: 1,
-          name: 'Infosys',
-          website: 'https://infosys.com',
-          image: 'src/assets/infosys.svg',
-          subtitle: 'Teachnology Lead',
-          date: 'June 2018 - February 2023',
-          description: 'As a Java Developer, I worked on an Agile team to design and implement scalable Java APIs for American Express to maintain and enhance their product catalog. Utilizing Vert.x and Couchbase, we were able to achieve high performance and customization, as well as ensure data persistence across multiple data centers. Collaborated with cross-functional teams and departments globally to deliver impactful features and provide ongoing support for these applications.',
-        },
-        {
-          id: 2,
-          name: 'Revature',
-          website: "https://revature.com",
-          image: 'src/assets/revature.png',
-          subtitle: 'Software Engineer',
-          date: 'February 2018 - August 2023',
-          description: 'As a Full-Stack Developer at Revature, I gained experience in various in-demand technologies, including Java, Spring Boot, Kafka and SQL databases for API development, and Angular for front-end. Revature\'s training aims to prepare Developers for job opportunities through full-stack proficiency.',
-        }
-      ]
-    },
-    {
-      id: 1,
-      title: "Education",
-      entries: [
-        {
-          id: 0,
-          name: 'Gordon College',
-          website: 'https://gordon.edu',
-          image: 'src/assets/gordon.png',
-          subtitle: 'Wenham, MA',
-          date: 'August 2013 - May 2017',
-          description: 'Bachelor of Science in Methematics with Minor in Computer Science',
-        }
-      ]
-    },
-    {
-      id: 2,
-      title: "Certifications",
-      entries: [
-        {
-          id: 0,
-          name: "Oracle Certified Associate",
-          website: 'https://www.credly.com/badges/e552e4c2-72ca-4042-9782-e40423780e3d/public_url',
-          subtitle: 'Oracle Certified Associate, Java SE 8 programmer',
-          image: 'src/assets/java-cert.png',
-          date: 'April 20, 2018',
-          description: 'An Oracle Certified Associate, Java SE 8 Programmer has demonstrated knowledge of object-oriented concepts, the Java programming language and general knowledge of Java platforms and technologies.',
-        }
-      ]
-    }
-  ]
 
   window.addEventListener('scroll', () => {
     document.querySelectorAll('.column-container').forEach(container => {
@@ -97,14 +32,14 @@ function App() {
           collaborating with cross-functional groups to troubleshoot, innovate, and deliver impactful features across the full stack.
         </p>
       </div>
-      {sections.map((sec) =>
-        <div key={sec.id}>
+      {sections.map((sec, secIndex) =>
+        <div key={secIndex}>
           <h2>{sec.title}</h2>
           <hr className="divider"></hr>
           <div className="card">
             <ul>
-              {sec.entries.map((entry) => (
-                <li key={entry.id}>
+              {sec.entries.map((entry, entryIndex) => (
+                <li key={entryIndex}>
                   <div className="column-container" id={`column-container-${sec.id}-${entry.id}`}>
                     <div className="column left">
                       <a href={entry.website} target="_blank">
