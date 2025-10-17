32c9efb46a91876dd609f384ed7a700fc42cc1a1
James Kempf <jamesk4321@gmail.com>
2025-10-17 10:53:25 -0500

    move images to public image folder

--- src/Entry.jsx
+++ src/Entry.jsx
@@ -6,7 +6,7 @@ function Entry({ entry }) {
         <div className='column-container'>
           <div className='column left'>
             <a href={entry.website} target='_blank'>
-              <img src={entry.image} href={entry.website} target='_blank' className='external-logo'></img>
+              <img src={entry.image} alt={entry.name} className='external-logo'></img>
             </a>
             <h4>{entry.subtitle}</h4>
             <p>{entry.date}</p>
